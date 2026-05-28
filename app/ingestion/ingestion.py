95bd08f9e83204ccc54e0dcb822316d21e1617c0
Venkatesh Thanikachalam <venky.thaniks@gmail.com>
2026-05-28 07:36:06 +0000

    Added the new files for the project ingestion folder

---
+++ app/ingestion/ingestion.py
@@ -0,0 +1,79 @@
+from dotenv import load_dotenv
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.core.db import get_vector_store
+import time
+
+# C:\Users\t91-labuser061568\Documents\agentic-ai-course-may2026\insurance_ai_project\Instructions_pdf
+load_dotenv()
+PG_CONNECTION = os.getenv("PG_CONNECTION_STRING")
+
+
+def ingest_pdf(file_path):
+   print("Ingestion Started")
+   #1. Load PDF
+   loader = PyPDFLoader(file_path)
+   docs = loader.load()
+   for i, doc in enumerate(docs):
+    text = doc.page_content or ""
+    print(f"Page {i+1}: chars={len(text)}")
+   
+   
+   print("Pages : ", len(docs))
+
+   # 2. Metadata enrichment
+   for doc in docs:
+       doc.metadata.update({
+           "source": file_path,
+           "document_extension": "pdf",
+           "page": doc.metadata.get("page"),
+           "last_updated": os.path.getmtime(file_path)
+       })
+
+   # 3. Chunking
+   splitter = RecursiveCharacterTextSplitter(
+       chunk_size=1000, # characters
+       chunk_overlap=200 # characters
+   )
+
+   chunks = splitter.split_documents(docs)
+   print("Total Chunks", len(chunks))
+
+   # 4 and 5
+   # generate the embeddings store in vector db
+   vector_store = get_vector_store(collection_name="insurance_support_desk", pre_delete_collection=True)
+   # FIXME: I am running a  for loop to add documents with ids. but it should ideally work with batch add_documents.
+   for i, chunk in enumerate(chunks):
+       vector_store.add_documents([chunk], ids=[f"{chunk.metadata['source']}_{chunk.metadata['page']}_{i}"])
+    #    time.sleep(3)   
+#    for i, chunk in enumerate(chunks):
+#     try:
+#         vector_store.add_documents(
+#             [chunk],
+#             ids=[f"{chunk.metadata['source']}_{chunk.metadata['page']}_{i}"]
+#         )
+#         print(f"Added document index {i} to DB")
+#     except Exception as e:
+#         print(f"Failed to add document index {i}: {e}")
+   
+#    for i, chunk in enumerate(chunks):
+#        vector_store.add_documents([chunk], ids=[f"{chunk.metadata['source']}_{chunk.metadata['page']}_{i}"])
+
+#    for i, chunk in enumerate(chunks):
+#        try:
+#          vector_store.add_documents([chunk], ids=[f"{chunk.metadata['source']}_{chunk.metadata['page']}_{i}")
+#          print(f"Added chunk {i} to DB")
+#        except Exception as e:
+#           print(f"Error addind chunk {i}:{e}")
+#    vector_store.add_documents(chunks)
+
+
+
+
+
+   print("======Ingestion Completed Successfully!=======")
+
+
+# if __name__ == "__main__":
+#    ingest_pdf("data/HR_Support_Desk_KnowledgeBase.pdf")
